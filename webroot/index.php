8382e3b0bdb366e1daf5c32694319a3693623d39
igwe-kanu <cikkanu@gmail.com>
2018-07-15 18:39:15 +0100

    initial commit

--- webroot/index.php
+++ webroot/index.php
@@ -1,40 +1,165 @@
-<?php
-/**
- * The Front Controller for handling every request
- *
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.2.9
- * @license       MIT License (https://opensource.org/licenses/mit-license.php)
- */
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>DE-BRIDGE ELITE CONSULTING</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
 
-// Check platform requirements
-require dirname(__DIR__) . '/config/requirements.php';
+		<!-- Header -->
+			<header id="header">
+				<a class="logo" href="index.html">DE-BRIDGE ELITE CONSULTING</a>
+				<nav>
+					<a href="#menu">Menu</a>
+				</nav>
+			</header>
 
-// For built-in server
-if (php_sapi_name() === 'cli-server') {
-    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="generic.html">Business Advisory</a></li>
+						<li><a href="govern.html">Governance & Finance</a></li>
+							<li><a href="humancpt.html">Human Capital Management</a></li>
+								<li><a href="learn.html">Learning Academy</a></li>
+									<li><a href="tech.html">Technology</a></li>
+				</ul>
+			</nav>
 
-    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
-    $file = __DIR__ . $url['path'];
-    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
-        return false;
-    }
-}
-require dirname(__DIR__) . '/vendor/autoload.php';
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h1>DE-BRIDGE ELITE CONSULTING</h1>
+					<p>At DBE, excellence with integrity is what we strive for and achieve consistently. We display our exceptional business acumen in providing you high-quality tailor-made solutions. We consistently empower our clientele with technology, software, advisory, HR services, and outsourcing solutions.</p>
+				</div>
+				<video autoplay loop muted playsinline src="images/banner.mp4"></video>
+			</section>
 
-use App\Application;
-use Cake\Http\Server;
+		<!-- Highlights -->
+			<section class="wrapper">
+				<div class="inner">
+					<header class="special">
+						<h2>SITE MAP</h2>
+						</header>
+					<div class="highlights">
+						<section>
+							<div class="content">
+								<header>
+									<a href="executives.html" class="icon fa-vcard-o"><span class="label">Icon</span></a>
+									<h3>Executive Team</h3>
+								</header>
+								<p>We are committed to creating solutions, developing methodologies, nurturing talent, implementing processes and fine-tuning organizational set-up.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="about.html" class="icon fa-files-o"><span class="label">Icon</span></a>
+									<h3>About Us</h3>
+								</header>
+								<p>We are an indigenous management and business advisory firm, founded in 2004, catering to clients across Africa, Asia, Europe and UAE. We have our local offices in Abuja, Port-Harcourt, Lagos, Kano & Kaduna.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="leatec.html" class="icon fa-floppy-o"><span class="label">Icon</span></a>
+									<h3>Learning Technology</h3>
+								</header>
+								<p>DE-BRIDGE ELITE  Learning Academy e-Learning Solutions was originally developed to meet the demanding requirements of talent assessment and compliance assessment.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="services.html" class="icon fa-line-chart"><span class="label">Icon</span></a>
+									<h3>Our Services</h3>
+								</header>
+								<p>We consistently empower our clientele with technology, software, advisory, HR services, and outsourcing solutions.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="contact.html" class="icon fa-paper-plane-o"><span class="label">Icon</span></a>
+									<h3>Contact Us</h3>
+								</header>
+								<p>You may reach us at any time using this contact form and at your convenience.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="wwd.html" class="icon fa-qrcode"><span class="label">Icon</span></a>
+									<h3>What We Do</h3>
+								</header>
+								<p>We help clients in developing a best-fit and sustainable Knowledge Management Framework.</p>
+							</div>
+						</section>
+					</div>
+				</div>
+			</section>
 
-// Bind your application to the server.
-$server = new Server(new Application(dirname(__DIR__) . '/config'));
+		<!-- CTA -->
+			<section id="cta" class="wrapper">
+				<div class="inner">
+					<h2>Welcome to De-Bridge Elite Consulting</h2>
+					<p>We never lose sight that people, systems, strategy and structure are crucial to the success of our clients. Therefore, we are committed to creating solutions, developing methodologies, nurturing talent, implementing processes and fine-tuning organizational set-up. We help our clients discover exceptional leaders, winning teams & successful organizations.</p>
+				</div>
+			</section>
 
-// Run the request/response through the application and emit the response.
-$server->emit($server->run());
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<div class="content">
+						<section>
+							<h3>WHY DE- BRIDGE ELITE CONSULTING?</h3>
+							<p>Most organizations are challenged today in one way or the other. From employee staffing, manual processes, poor inter-departmental communication, inefficient operational activities, wrong hiring processes, lack of automation, cumbersome, poor employee utilization and unproductive workflows to non-existent & ineffective technology infrastructure.
+</p><p>This is where DBE comes in!</br>
+DBE offers you critical tools, technical expertise and time-tested methodologies. We partner with clients to construct the right solutions for their specific challenges in each of these areas and more. Our clients testify of our professionalism, ability to deliver quality and on time. We help build, implement and sustain internal capabilities to ensure that they are enabled to manage organizational changes successfully in the future.
+
+</p>
+						</section>
+						<section>
+							<h4>Popular Links</h4>
+							<ul class="alt">
+								<li><a href="govern.html">Governance & Finance</a></li>
+									<li><a href="humancpt.html">Human Capital Management</a></li>
+										<li><a href="learn.html">Learning Academy</a></li>
+										<li><a href="hucama.html">Human Capital Management</a></li>
+											<li><a href="tech.html">Technology</a></li>
+											<li><a href="perfman.html">Performance Management And Performance Improvement</a></li>
+											<li><a href="cpgvdv.html">Corporate Governance Development And Review</a></li>
+											<li><a href="manret.html">Management Retreats</a></li>
+							</ul>
+						</section>
+						<section>
+							<h4>SOCIAL MEDIA HANDLES</h4>
+							<ul class="plain">
+								<li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
+								<li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
+								<li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
+								<li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li>
+							</ul>
+						</section>
+					</div>
+					<div class="copyright">
+						&copy; 2018. DE-BRIDGE ELITE CONSULTING
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
